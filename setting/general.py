9122dfa838926d2b71fcc9379c71982f0c6c4216
Faisyah Dheana Ratna Dewi <faisyahdheanard@gmail.com>
2023-09-29 21:46:43 +0700

    edit(5) add send notif to slack

--- setting/general.py
+++ setting/general.py
@@ -3,4 +3,4 @@ import os
 
 max_latency = 1000000
 api_key = "4e1dc896a03e54b1f0a4081fce5b1c0a"
-webhook_slack = os.environ.get("PYTEST_WEBHOOK_SLACK")
+webhook_slack = os.environ.get('PYTEST_WEBHOOK_SLACK')
